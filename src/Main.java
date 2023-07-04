90739c7ca4a8cacd42a2311a6d487360ad215a11
ahmetulvi <47754871+ahmetulvi@users.noreply.github.com>
2023-07-05 00:54:46 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      int[][] matrix=   {{5,6,7 },{8,9,10}};
+        System.out.println("Before the change.");
+        print(matrix);
+        System.out.println("------------------");
+        System.out.println("After the change.");
+        change(matrix);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
+    public static void change(int[][] matrix){
+        int[][] newMatrix=new int[matrix[0].length][matrix.length];
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (int rows=0;rows< matrix.length;rows++){
+            for (int col=0;col< matrix[0].length;col++){
+                newMatrix[col][rows]=matrix[rows][col];
+            }
+        }
+        print(newMatrix);
+
+    }
+    public static void print(int[][] matrix){
+        for (int [] rows : matrix) {
+            for (int col : rows) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
         }
     }
 }
